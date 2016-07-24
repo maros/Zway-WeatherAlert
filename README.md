f90cb23af45d37c1c1a9a9776668016eb5d38dfc
Maros Kollar <maros@k-1.com>
2016-07-24 10:15:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Zway-WeatherAlert
+
+Zway automation base module for severe weather alerts
+
+# Configuration
+
+## latitude,longitude
+
+Geo coordinates for the used position
+
+# Events
+
+TODO
+
+# Virtual Devices
+
+This module creates a virtual binary sensor that displays the current alert
+situation.
+
+# Installation
+
+Install the BaseModule from https://github.com/maros/Zway-BaseModule first
+
+The prefered way of installing this module is via the "Zwave.me App Store"
+available in 2.2.0 and higher. For stable module releases no access token is 
+required. If you want to test the latest pre-releases use 'k1_beta' as 
+app store access token.
+
+For developers and users of older Zway versions installation via git is 
+recommended.
+
+```shell
+cd /opt/z-way-server/automation/userModules
+git clone https://github.com/maros/Zway-RemoteHost.git RemoteHost --branch latest
+```
+
+To update or install a specific version
+```shell
+cd /opt/z-way-server/automation/userModules/RemoteHost
+git fetch --tags
+# For latest released version
+git checkout tags/latest
+# For a specific version
+git checkout tags/1.02
+# For development version
+git checkout -b master --track origin/master
+```
+
+# License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any 
+later version.
+
+Alert icon by Thomas Le Bas from the Noun Project
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
---
+++ index.js
@@ -0,0 +1,236 @@
+/*** WeatherAlert Z-Way HA module *******************************************
+
+Version: 1.00
+(c) Maroš Kollár, 2015
+-----------------------------------------------------------------------------
+Author: Maroš Kollár <maros@k-1.com>
+Description:
+    WeatherAlert module
+
+******************************************************************************/
+
+function WeatherAlert (id, controller) {
+    // Call superconstructor first (AutomationModule)
+    WeatherAlert.super_.call(this, id, controller);
+    
+    this.baseurl    = 'http://feed.alertspro.meteogroup.com/AlertsPro/AlertsProPollService.php';
+    this.severity   = {
+        "green":    0,
+        "yellow":   2,
+        "orange":   3,
+        "red":      4,
+        "violet":   5
+    };
+    this.type      = [
+        "unknown",
+        "storm",
+        "snow",
+        "rain",
+        "frost",
+        "forest_fire",
+        "thunderstorm",
+        "glaze",
+        "heat",
+        "freezing_rain",
+        "soil_frost"
+    ];
+    
+    this.areaId     = undefined;
+}
+
+inherits(WeatherAlert, BaseModule);
+
+_module = WeatherAlert;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+WeatherAlert.prototype.init = function (config) {
+    WeatherAlert.super_.prototype.init.call(this, config);
+    
+    var self = this;
+    
+    // Create vdev
+    self.vDev = self.controller.devices.create({
+        deviceId: "WeatherAlert_" + self.id,
+        defaults: {
+            metrics: {
+                timestamp: 0,
+                title: self.langFile.m_title,
+                level: 0,
+                type: null,
+                text: null,
+                icon: self.imagePath+'/icon_severity0.png'
+            }
+        },
+        overlay: {
+            //alertType: self.config.type,
+            probeType: 'weather_alert',
+            deviceType: 'sensorMultilevel'
+        },
+        handler: function(command,args) {
+            if (command === 'update') {
+                var timestamp = this.get('metrics:timestamp') - (10 * 60);
+                if (timestamp < parseInt((new Date()).getTime() / 1000)) {
+                    //self.getAlerts();
+                }
+            }
+        },
+        moduleId: self.id
+    });
+    
+    setTimeout(_.bind(self.getAreaId,self),1000 * 15);
+    self.interval = setInterval(_.bind(self.getAlerts,self,'interval'),30*60*1000);
+};
+
+WeatherAlert.prototype.stop = function () {
+    var self = this;
+    
+    if (self.vDev) {
+        self.controller.devices.remove(self.vDev.id);
+        self.vDev = undefined;
+    }
+    
+    if (typeof(self.interval) !== 'undefined') {
+        clearInterval(self.interval);
+        self.interval = undefined;
+    }
+    
+    WeatherAlert.super_.prototype.stop.call(this);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+WeatherAlert.prototype.query = function(method,args,callback) {
+    var self = this;
+    var url = self.baseurl+'?method='+method;
+    _.each(args,function(value,key){
+        url = url + '&' + key + '=' + value;
+    });
+    
+    self.log(url);
+    http.request({
+        url: url,
+        async: true,
+        success: function(response) {
+            if (typeof(response.data) === 'string') {
+                callback(JSON.parse(response.data));
+            } else {
+                callback(response.data);
+            }
+        },
+        error: function(response) {
+            self.error("Update error: "+response.statusText);
+            console.logJS(response);
+            self.controller.addNotification(
+                "error", 
+                self.langFile.error_fetch,
+                "module", 
+                self.constructor.name
+            );
+        }
+    });
+};
+
+WeatherAlert.prototype.getAreaId = function() {
+    var self = this;
+    
+    if (typeof(self.config.latitude) === 'undefined'
+        || typeof(self.config.longitude) === 'undefined') {
+        self.error('Latitude and/or longitude missing');
+        return;
+    }
+    
+    self.log('Fetch Area ID');
+    // Get coordinates
+    self.query(
+        'lookupCoord',
+        { lat: self.config.latitude, lon: self.config.longitude },
+        function(response) {
+            self.areaId = response[0].AREA_ID;
+            if (typeof(self.areaId) !== 'undefined') {
+                self.getAlerts();
+            }
+        }
+    );
+};
+
+WeatherAlert.prototype.getAlerts = function() {
+    var self = this;
+    
+    if (typeof(self.areaId) === 'undefined') {
+        self.log('No Area ID available yet');
+        self.getAreaId();
+        return;
+    }
+    
+    var alerts      = [];
+    self.log('Fetch alerts for areaID '+self.areaId);
+    
+    self.query(
+        'getWarning',
+        { areaID: self.areaId, language: self.controller.defaultLang },
+        _.bind(self.processAlerts,self)
+    );
+};
+
+WeatherAlert.prototype.processAlerts = function(response) {
+    var self = this;
+    
+    var severity = 0;
+    var type = null;
+    var text = null;
+    var currentTime = parseInt((new Date()).getTime() / 1000);
+
+    self.log('Process alerts');
+    _.each(response.results,function(result) {
+        console.logJS(result);
+        if (result.dtgStart > currentTime
+            || result.dtgEnd < currentTime) {
+            self.log('SKIP TIME');
+            return;
+        }
+        if (typeof(self.config.altitude) !== 'undefined'
+            && (self.config.altitude < result.payload.altMin || self.config.altitude > result.payload.altMax)) {
+            self.log('SKIP ALT');
+            return;
+        }
+        
+        if (typeof(self.config.type) !== 'undefined'
+            && self.config.type.length > 0
+            && _.indexOf(self.config.type,self.type[ result.type - 1 ]) === -1) {
+            self.log('SKIP TYPE'+result.type+' -> '+self.type[ result.type - 1 ]);
+            return;
+        }
+        
+        var resultSeverity = self.getSeverity(result.payload.levelName);
+        self.log(result.payload.levelName+'->'+resultSeverity+'->'+self.type[ result.type - 1 ]);
+        if (resultSeverity > alert.severity) {
+            severity  = resultSeverity;
+            type      = self.type[ result.type - 1 ];
+            text      = result.payload.shortText;
+        }
+    });
+    
+    self.vDev.set("metrics:timestamp",currentTime);
+    self.vDev.set('metrics:level',severity);
+    self.vDev.set('metrics:icon',self.imagePath+'/icon_severity'+severity+'.png');
+    self.vDev.set('metrics:type',type);
+    self.vDev.set('metrics:type',text);
+};
+
+WeatherAlert.prototype.getSeverity = function(levelName) {
+    var levels = levelName.split('_');
+    if (levels[0] === 'notice') {
+        return 1;
+    } else if (levels[1] === 'forewarn') {
+        return 2;
+    } else {
+        return self.severity[levels[2]];
+    }
+    self.log('Could not parse levelName: '+levelName);
+    return 0;
+};
